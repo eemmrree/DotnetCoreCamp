0cf0be56b7c06f8af2a7aaf7d6c95b1a47a85c12
Emre <eemmrree0@outlook.com>
2021-10-20 21:23:24 +0300

    Day3

---
+++ Lessons/Day3_Methods/BasketManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Methods
+{
+    class BasketManager
+    {
+        public void AddProduct(Product product)
+        {
+            Console.WriteLine("Added  :" + product.ProductName);
+        }
+        public void AddProduct2(string productName , string description , int price)
+        {
+            Console.WriteLine("New Added2  :" +productName);
+            Console.WriteLine("New Added2 : " +description);
+            Console.WriteLine("New Added2 : " +price);
+        }
+    }
+}
---
+++ Lessons/Day3_Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            var p1 = new Product() {Id = 1, ProductName = "Elma", Description = "Kırmızı Elma", Price = 12};
+            var p2 = new Product() {Id = 2, ProductName = "Kiraz", Description = "Yeşil Elma", Price = 10};
+            var products = new Product[] { p1, p2 };
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Id+" "+item.Description + " ---> " + item.Price);
+            }
+
+            var bm = new BasketManager();
+            bm.AddProduct(p2);
+            bm.AddProduct2("Elma" ,"Deneme" , 15);
+        }
+    }
+}
