8122b4a32fba74914b3ea191222592a6c3241290
Emre <eemmrree0@outlook.com>
2021-10-20 20:02:58 +0300

    Day2

---
+++ Lessons/Day2_Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day2_Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string adi = "emre";
+            //int yas = 23;
+
+            var k1 = new Kurs { KursAdi = "C#", Egitmen = "Emre", Izlenme = 56 };
+
+            var k2 = new Kurs { KursAdi = "Python", Egitmen = "Emre", Izlenme = 15 };
+
+            var k3 = new Kurs { KursAdi = "Java", Egitmen = "Emre", Izlenme = 23 };
+
+            Console.WriteLine(k1.KursAdi + " - " + k1.Egitmen);
+
+            var kurs = new Kurs[] { k1, k2, k3 };
+
+            foreach (var item in kurs)
+            {
+                Console.WriteLine(item.KursAdi);
+            }
+        }
+    }
+    public class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int Izlenme { get; set; }
+    }
+}
