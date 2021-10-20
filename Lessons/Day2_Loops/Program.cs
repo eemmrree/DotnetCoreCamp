bfbf3a96d06c2bb9e613f6b9e73b94e79cd52a1d
Emre <eemmrree0@outlook.com>
2021-10-20 20:03:04 +0300

    Day2

---
+++ Lessons/Day2_Loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day2_Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            //array
+
+            string[] kurslar = new[] { "Ağ", "C#", "C++" };
+
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            //for
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (var model in kurslar)
+            {
+                // Console.WriteLine(model.Length);
+                Console.WriteLine(model);
+
+            }
+
+            Console.WriteLine("---Sayfa Sonu---");
+        }
+    }
+}
