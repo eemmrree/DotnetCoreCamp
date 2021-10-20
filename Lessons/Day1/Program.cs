8b7ff98558aebc94d6f33ea90d61d9b573ee62fe
Emre <eemmrree0@outlook.com>
2021-10-20 19:56:44 +0300

    Add project files.

---
+++ Lessons/Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double dolarBugun = 7.35;
+            double dolarDun = 7.45;
+            bool girisDenetim = false;
+            if (dolarBugun > dolarDun)
+            {
+                Console.WriteLine("Artış!");
+            }
+            else if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış!");
+            }
+            else
+            {
+                Console.WriteLine("Eşit");
+            }
+            if (girisDenetim == false)
+            {
+                Console.WriteLine("Başarısız Giriş!");
+            }
+        }
+    }
+}
