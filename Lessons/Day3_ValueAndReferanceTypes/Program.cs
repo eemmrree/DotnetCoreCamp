1d005e4b850d6704a5b2041635de591021aec292
Emre <eemmrree0@outlook.com>
2021-10-20 21:23:30 +0300

    Day3

---
+++ Lessons/Day3_ValueAndReferanceTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day3_ValueAndReferanceTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int x = 5;
+            int y = 10;
+
+            x = y;
+            y = 15;
+
+
+            int[] values = new[] {10, 20, 30};
+            int[] values2 = new[] {100, 200, 300};
+
+            values = values2;
+            values2[0] = 999;
+            
+            
+            Console.WriteLine(x);
+            Console.WriteLine(values[0]);
+
+        }
+    }
+}
