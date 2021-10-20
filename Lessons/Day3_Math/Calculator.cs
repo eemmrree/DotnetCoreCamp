7ae5d7e83fda94e3a7b6bece3149e80de729304f
Emre <eemmrree0@outlook.com>
2021-10-20 21:23:16 +0300

    Day3

---
+++ Lessons/Day3_Math/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Math
+{
+    class Calculator
+    {
+        public void Sum(int x , int y)
+        {
+            int sum = x + y;
+            Console.WriteLine(" => " +sum);
+        }
+    }
+}
---
+++ Lessons/Day3_Math/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day3_Math
+{
+    class Program
+    {
+        static void Main()
+        {
+            var c = new Calculator();
+            c.Sum(5,6);
+
+            c.Sum(2,50);
+        }
+    }
+}
