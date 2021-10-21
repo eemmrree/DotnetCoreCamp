fcf6c00e7d25fe8c64df4b50da8814b16aab1241
Emre <eemmrree0@outlook.com>
2021-10-21 17:40:22 +0300

    Day4

---
+++ Lessons/Day4_OOP1/ProductManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            
+            //product.ProductName = "Kamera";
+            Console.WriteLine(product.ProductName + " Eklendi..");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi..");
+        }
+
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public void Sum2(int x, int y)
+        {
+            Console.WriteLine(x + y); 
+        }
+
+
+
+
+        //public void Test(int sayi)
+        //{
+        //    sayi = 99;
+        //}
+    }
+}
---
+++ Lessons/Day4_OOP1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Day4_OOP1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Product p1 = new Product
+            {
+                Id = 1,
+                CategoryId = 2,
+                ProductName = "Masa",
+                UnitPrice = 500,
+                UnitInStock = 3
+            };
+
+            Product p2 = new Product
+            {
+                Id = 2,
+                CategoryId = 1,
+                ProductName = "Kalem",
+                UnitPrice = 5,
+                UnitInStock = 3
+            };
+
+            //PascalCase   //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(p1);
+
+            //Console.WriteLine(p1.ProductName);
+            //Output -> Kamera
+
+
+
+
+            productManager.Sum2(3,4);
+            int result = productManager.Sum(3, 7);
+            Console.WriteLine(result*2);
+
+
+
+            //int sayi = 100;
+            //productManager.Test(sayi);
+
+            //Console.WriteLine(sayi);
+            //Output -> 100
+
+        }
+    }
+}
