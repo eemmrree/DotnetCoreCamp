dfc8e7584a608ce1d8342509c654f27fd70989c6
Emre <eemmrree0@outlook.com>
2021-10-21 19:00:03 +0300

    Day4

---
+++ Lessons/Day4_GenericsIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day4_GenericsIntro
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyList<int> numbers = new MyList<int>();
+            numbers.Add(4);
+            numbers.Add(3);
+            numbers.Add(5);
+            foreach (var item in numbers.Items)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine(numbers.Length);
+            
+
+        }
+    }
+}
