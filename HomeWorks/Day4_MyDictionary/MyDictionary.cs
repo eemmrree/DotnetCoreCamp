ec2ad7829c9f402451a3bd19823f2b8fff13579f
Emre <eemmrree0@outlook.com>
2021-10-22 13:42:56 +0300

    Day4_Homework

---
+++ HomeWorks/Day4_MyDictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_MyDictionary
+{
+    class MyDictionary<TKey , TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < keys.Length - 1; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+
+        }
+        public TValue[] Items
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ HomeWorks/Day4_MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day4_MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> names = new MyDictionary<int, string>();
+            names.Add(1, "Emre");
+            names.Add(2, "Esra");
+
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine(names.Length);
+        }
+    }
+}
